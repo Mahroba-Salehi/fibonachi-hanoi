f4e16825ae06c872f0faaa9ccb3cbbf568166524
mahroba <mahroba.slh@gmail.com>
2025-10-19 11:36:34 +0330

    updated

--- source/utils.py
+++
@@ -1,7 +0,0 @@
-# utils.py
-import datetime
-
-def log_event(message, logfile="log.txt"):
-    timestamp = datetime.datetime.now().strftime("[%H:%M:%S]")
-    with open(logfile, "a", encoding="utf-8") as f:
-        f.write(f"{timestamp} {message}\n")
